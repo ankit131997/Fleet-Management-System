60cd9cffaa21a2a1747b708480a027d9db541818
Mihir Shrangi <127875527+MihirShrangi@users.noreply.github.com>
2025-02-06 12:14:49 +0530

    Update StateMaster.java

--- FleeMan/src/main/java/com/example/demo/Entity/StateMaster.java
+++ FleeMan/src/main/java/com/example/demo/Entity/StateMaster.java
@@ -21,4 +21,20 @@ public class StateMaster {
 	@Column(name = "state_name")
     private String stateName;
 
+	public int getStateId() {
+		return stateId;
+	}
+
+	public void setStateId(int stateId) {
+		this.stateId = stateId;
+	}
+
+	public String getStateName() {
+		return stateName;
+	}
+
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+
 }
