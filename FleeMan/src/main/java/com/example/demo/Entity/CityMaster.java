c56961b211a23a233e9a88582e7de65e0222c475
Mihir Shrangi <127875527+MihirShrangi@users.noreply.github.com>
2025-02-06 12:08:51 +0530

    Update CityMaster.java

--- FleeMan/src/main/java/com/example/demo/Entity/CityMaster.java
+++ FleeMan/src/main/java/com/example/demo/Entity/CityMaster.java
@@ -22,11 +22,34 @@ public class CityMaster {
     @Column(name = "city_id")
     private int cityId;
 	
+	public int getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(int cityId) {
+		this.cityId = cityId;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public StateMaster getState() {
+		return state;
+	}
+
+	public void setState(StateMaster state) {
+		this.state = state;
+	}
+
 	@Column(name = "city_name")
 	private String cityName;
 	
 	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "state_id")
     private StateMaster state;
-
 }
