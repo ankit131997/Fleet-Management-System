8ecbc85fbe1ad00ce5dc507012499bd18046e97e
Mihir Shrangi <127875527+MihirShrangi@users.noreply.github.com>
2025-02-06 12:13:06 +0530

    Update InvoiceHeaderTable.java

--- FleeMan/src/main/java/com/example/demo/Entity/InvoiceHeaderTable.java
+++ FleeMan/src/main/java/com/example/demo/Entity/InvoiceHeaderTable.java
@@ -50,5 +50,102 @@ public class InvoiceHeaderTable {
     @Column(name = "rate")
     private String rate;
 
+	public long getInvoiceId() {
+		return invoiceId;
+	}
+
+	public void setInvoiceId(long invoiceId) {
+		this.invoiceId = invoiceId;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public BookingHeaderTable getBooking() {
+		return booking;
+	}
+
+	public void setBooking(BookingHeaderTable booking) {
+		this.booking = booking;
+	}
+
+	public CustomerMaster getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(CustomerMaster customer) {
+		this.customer = customer;
+	}
+
+	public LocalDate getHandoverDate() {
+		return handoverDate;
+	}
+
+	public void setHandoverDate(LocalDate handoverDate) {
+		this.handoverDate = handoverDate;
+	}
+
+	public CarMaster getCar() {
+		return car;
+	}
+
+	public void setCar(CarMaster car) {
+		this.car = car;
+	}
+
+	public LocalDate getReturnDate() {
+		return returnDate;
+	}
+
+	public void setReturnDate(LocalDate returnDate) {
+		this.returnDate = returnDate;
+	}
+
+	public double getRentalAmt() {
+		return rentalAmt;
+	}
+
+	public void setRentalAmt(double rentalAmt) {
+		this.rentalAmt = rentalAmt;
+	}
+
+	public double getTotalAddonAmt() {
+		return totalAddonAmt;
+	}
+
+	public void setTotalAddonAmt(double totalAddonAmt) {
+		this.totalAddonAmt = totalAddonAmt;
+	}
+
+	public double getTotalAmt() {
+		return totalAmt;
+	}
+
+	public void setTotalAmt(double totalAmt) {
+		this.totalAmt = totalAmt;
+	}
+
+	public String getCustomerDetails() {
+		return customerDetails;
+	}
+
+	public void setCustomerDetails(String customerDetails) {
+		this.customerDetails = customerDetails;
+	}
+
+	public String getRate() {
+		return rate;
+	}
+
+	public void setRate(String rate) {
+		this.rate = rate;
+	}
+
+
     
 }
