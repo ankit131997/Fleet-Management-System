487c820337e0e8bd80c7bc2fd261117ec95d9c05
ankit131997 <ankitkr.131997@gmail.com>
2025-03-17 19:12:36 +0530

    Updated project files

--- Fleeman_Dotnet/obj/Debug/net8.0/Fleeman_Dotnet.AssemblyInfo.cs
+++ Fleeman_Dotnet/obj/Debug/net8.0/Fleeman_Dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fleeman_Dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b95894a451ec09df0257f666d0413dcefcfa5ef4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273686cc0226f2953952a8cd0bd4305467f903a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fleeman_Dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fleeman_Dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
